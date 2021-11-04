1d63bdeb6dcf4381fb090ecf8072c81ad8589352
Mateus dos Santos <loiolamateus7@gmail.com>
2021-11-04 16:37:55 -0300

    New transaction modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,59 @@
+import Modal from 'react-modal'
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './styles'
+
+interface NewTransactionModalProps {
+  isOpen: boolean,
+  onRequestClose: () => void
+}
+
+Modal.setAppElement('#root')
+
+export function NewTransactionModal({
+  isOpen, onRequestClose
+}: NewTransactionModalProps) {
+  return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+        <button 
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={closeImg} alt="Fechar"/>
+        </button>
+        <Container>
+          <h2>Cadastrar transação</h2>
+
+          <input placeholder="Título" />
+
+          <input placeholder="Valor" type="number" />
+
+          <TransactionTypeContainer>
+            <button 
+              type="button"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entranda</span>
+            </button>
+            <button 
+              type="button"
+            >
+              <img src={outcomeImg} alt="Saída" />
+              <span>Saída</span>
+            </button>
+          </TransactionTypeContainer>
+
+          <input placeholder="Categoria" />
+
+          <button type="submit">Cadastrar</button>
+        </Container>
+      </Modal>
+  );
+}
