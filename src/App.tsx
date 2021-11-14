d15e856de8a1303a0a2fe9620590bd3ac34fe12a
Mateus dos Santos <loiolamateus7@gmail.com>
2021-11-14 19:28:29 -0300

    add context in application

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
 import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 export function App() {
   const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false)
@@ -17,7 +18,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <GlobalStyle />
       <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <NewTransactionModal
@@ -25,6 +26,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <Dashboard />
-    </>
+    </TransactionsProvider>
   );
 }
